db45db3256628ac74ec4c7b96894ba385a29c3e4
hassancodess <dev.hassancodes@gmail.com>
2022-08-12 22:10:17 +0500

    Add Mobile Menu

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+// Mobile Menu
+const menuBtn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+// Form
 const errMsg = document.getElementById('err-msg')
 const linkForm = document.getElementById('link-form')
 const linkInput = document.getElementById('link-input')
 
 linkForm.addEventListener('submit', formSubmit)
+menuBtn.addEventListener('click', navToggle)
 
+// on Menu Click
+function navToggle() {
+  console.log(123)
+  console.log(menu)
+  console.log(menuBtn)
+  menuBtn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
+
+// Form Submit
 function formSubmit(e) {
   e.preventDefault()
 
